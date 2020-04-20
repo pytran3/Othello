9d1b0a32465851d0e35f45381ea6fbaba8f5bc1a
pytran <34180469+pytran3@users.noreply.github.com>
2020-04-20 17:23:18 +0900

    fix evaluation.py

--- othello/benchmark/evaluation.py
+++ othello/benchmark/evaluation.py
@@ -1,14 +1,14 @@
-from othello.ai import MiniMaxAI, MonteCarloAI, RandomAI
+from othello.ai import MiniMaxAI, RandomAI
 from othello.helper import extract_valid_hand, put_and_reverse, judge_simple
 from othello.model import Board
 from othello.view import view_board
 
 
-def evaluate(sente=True, is_view=False):
+def evaluate(sente=False, is_view=False):
     board = Board.init_board()
     board.side = sente
     ai1 = RandomAI()
-    ai2 = MonteCarloAI(0, c=0.6)
+    ai2 = MiniMaxAI(5)
     if is_view:
         print(view_board(board))
     hands = []
@@ -17,33 +17,36 @@ def evaluate(sente=True, is_view=False):
             board.side ^= True
         if not extract_valid_hand(board):
             break
-        # if len(hands) >= 56:
-        #     hand = ai2.put(board, hands)
-        if board.side:
+        if board.side ^ sente:
             hand = ai1.put(board, hands)
         else:
             hand = ai2.put(board, hands)
+        if (board.board == 0).sum() < 12:
+            # 計算時間に余裕があるのでdeepに読む
+            ai2.depth = 8
         hands.append(hand)
         board = put_and_reverse(hand, board)
         if is_view:
             print(view_board(board))
+
 
     if is_view:
         print("=" * 10 + "  GAME OVER  " + "=" * 10)
         x_count = (board.board == 1).sum()
         o_count = (board.board == -1).sum()
         print("x: {}, o: {}".format(x_count, o_count))
-    return judge_simple(board)
+    return judge_simple(board) * (-1 if sente else 1)
 
 
 def main(play_count):
     result = [0, 0, 0]
     for i in range(play_count):
         result[evaluate(i % 2 == 0)] += 1
-        if i % (play_count // 10) == 9:
+        if i % (play_count // 10) == 0:
             print(result)
     print("AI 1 win: {}, lose: {}, even: {}".format(result[1], result[-1], result[0]))
 
 
 if __name__ == '__main__':
-    main(100)
+    main(50)
+    # evaluate(is_view=True)
